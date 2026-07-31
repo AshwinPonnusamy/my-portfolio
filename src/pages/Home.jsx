64e311322151be5d136c30c9c7cac7b3ec9fcb4d
AshwinPonnusamy <ashwin.ponnusamy@coherent.in>
2026-07-31 19:10:48 +0530

    feat: create Home page component with smooth hash-based scrolling functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export const Home = () => {
       <Hero />
       <Achievements />
       <About />
-      <Services />
+      {/* <Services /> */}
       <Portfolio />
       {/* <Testimonials /> */}
       <Contact />
